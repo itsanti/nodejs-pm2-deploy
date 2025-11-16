67c9756e52035c65ddc5bdbb51cd9c2b304ad7af
Aleksandr Kurov <a.kurov@gbc-team.com>
2025-11-16 18:08:09 +0300

    Deploy frontend

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require("dotenv").config({ path: "../.env.deploy" });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF,
+  DEPLOY_REPO,
+  DEPLOY_PATH_FRONTEND,
+} = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      ssh_options: "IdentityFile=~/.ssh/edvm",
+      "post-deploy": `cd ${DEPLOY_PATH_FRONTEND} && npm i && npm run build`,
+    },
+  },
+};
