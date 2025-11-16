e546ee34e83f8f5e90b922d4765ada85e24ce71d
Aleksandr Kurov <a.kurov@gbc-team.com>
2025-11-16 20:09:32 +0300

    add id file

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -30,7 +30,7 @@ module.exports = {
       env: {
         PATH: `/home/${DEPLOY_USER}/.nvm/versions/node/v20.19.5/bin:$PATH`,
       },
-      "pre-deploy-local": `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH_BACKEND}/.env`,
+      "pre-deploy-local": `scp -i ~/.ssh/edvm ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH_BACKEND}/.env`,
       "post-deploy": `cd ${DEPLOY_PATH_BACKEND} && npm i && npm run build && pm2 reload ${DEPLOY_PATH_BACKEND}/ecosystem.config.js --env production`,
     },
   },
